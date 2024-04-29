a5382c368afc7f9502448c5e0a1ce24f685efd71
Mario <mario300401@gmail.com>
2024-04-29 19:40:35 -0300

    Se completa la clase PagoEfectivo

--- tp2_MarioRios/src/ar/edu/unju/fi/ejercicio5/model/PagoEfectivo.java
+++ tp2_MarioRios/src/ar/edu/unju/fi/ejercicio5/model/PagoEfectivo.java
@@ -1,5 +1,26 @@
 package ar.edu.unju.fi.ejercicio5.model;
 
-public class PagoEfectivo {
+import java.time.LocalDate;
 
+import ar.edu.unju.fi.ejercicio5.interfaces.Pago;
+
+public class PagoEfectivo implements Pago{
+	private LocalDate fechaPago;
+    private double montoPagado;
+    
+	public PagoEfectivo(LocalDate fechaPago, double montoPagado) {
+		this.fechaPago = fechaPago;
+		this.montoPagado = montoPagado;
+	}
+	
+	@Override
+	public void realizarPago(double monto) {
+		this.montoPagado = monto - (monto * 0.10);
+	}
+	
+	@Override
+	public void imprimirRecibo() {
+		System.out.println("Fecha de pago: " + fechaPago);
+        System.out.println("Monto pagado: " + montoPagado);
+	}
 }
