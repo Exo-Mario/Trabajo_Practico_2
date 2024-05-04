2d8aa4cfcdd7a78777f54ac4056d701d84b45646
Mario <mario300401@gmail.com>
2024-05-04 17:52:04 -0300

    Se completa la clase Main [Fin Ejercicio 6]

--- tp2_MarioRios/src/ar/edu/unju/fi/ejercicio6/main/Main.java
+++ tp2_MarioRios/src/ar/edu/unju/fi/ejercicio6/main/Main.java
@@ -1,10 +1,24 @@
 package ar.edu.unju.fi.ejercicio6.main;
 
-public class Main {
+import ar.edu.unju.fi.ejercicio6.interfaces.funcionales.Converter;
+import ar.edu.unju.fi.ejercicio6.model.FelinoDomestico;
+import ar.edu.unju.fi.ejercicio6.model.FelinoSalvaje;
 
+public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		FelinoSalvaje felinoSalvaje = new FelinoSalvaje("Tanner", (byte) 20, 186f);
+		
+		// Verificar
+        if (Converter.isNotNull(felinoSalvaje)) {
+            // expresion Lambda
+            Converter<FelinoSalvaje, FelinoDomestico> converter = x -> new FelinoDomestico(
+                    x.getNombre(),
+                    x.getEdad(),
+                    x.getPeso()
+            ); 
+            FelinoDomestico felinoDomestico = converter.convert(felinoSalvaje);
+            converter.mostrarObjeto(felinoDomestico);
+        }
 	}
-
 }
